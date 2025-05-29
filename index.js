c92f5d9c7d0117cab3c37353bb3ac3aa8d515cc4
Santiago Cézar <santiagocezar2013@gmail.com>
2025-05-29 19:34:36 -0300

    devolver error como json

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import materias from './api/materias.js'
 import inasistencias from './api/inasistencias.js'
 import calificaciones from './api/calificaciones.js'
 import docentes from './api/docentes.js'
+import { error } from './api/common.js'
 
 const app = express()
 
@@ -15,6 +16,11 @@ app.use("/api/materias", materias)
 app.use("/api/inasistencias", inasistencias)
 app.use("/api/calificaciones", calificaciones)
 app.use("/api/docentes", docentes)
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  error(res, 500, err.message)
+})
 
 app.listen(3000, () => {
     console.log("Listening on port 3000")
