d44a57fa9754f958069d01bd8f43d6af1c10fe74
Santiago Cézar <santiagocezar2013@gmail.com>
2025-05-29 18:42:05 -0300

    decodificar json

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import docentes from './api/docentes.js'
 
 const app = express()
 
+app.use(express.json());
 app.use("/api/alumnos", alumnos)
 app.use("/api/materias", materias)
 app.use("/api/inasistencias", inasistencias)
