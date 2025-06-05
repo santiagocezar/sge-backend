124e9f2378466b14cb8cd8477e8ad431e347a2c5
alejo-pioli <apioli@facultad.sanfrancisco.utn.edu.ar>
2025-06-05 13:02:28 -0300

    añadir validacion con express validator

--- api/alumnos.js
+++ api/alumnos.js
@@ -3,27 +3,44 @@ import { Student } from "../db/index.js";
 
 const alumnos = Router()
 
-const alumnosdb = await Student.findAll()
-
 alumnos.route("/")
     .get(async (req, res) => {
         res.json(await Student.findAll())
     })
-    .post(async (req, res) => {
+    .post([
+        body("name").notEmpty().withMessage("Se requiere nombre"),
+        body("surname").notEmpty().withMessage("Se requiere apellido"),
+        body("email").isEmail().withMessage("Email no es válido"),
+        body("phone").isMobilePhone().withMessage("Teléfono no es válido")
+    ], async (req, res) => {
         const data = await Student.create(req.body)
-        res.status(201)
-        res.json({ ok: true, id: data.id })
+        res.status(201).json({ ok: true, id: data.id })
     })
 
 alumnos.route("/:id")
-    .get((req, res) => {
-
+    .get(async (req, res) => {
+        const data = await Student.findByPk(req.params.id);
+        if (data) {
+            res.status(200).json(data)
+        } else {
+            error(res, 404, "no existe el alumno");
+        }
     })
-    .put((req, res) => {
-
+    .put(async (req, res) => {
+        const [data] = await Student.upsert({ ...req.body, id: req.params.id })
+        res.status(200)
+        res.json({ ok: true, id: data.id })
     })
-    .delete((req, res) => {
-        
+    .delete(async (req, res) => {
+        const data = await Student.findByPk(req.params.id)
+
+        if (!data) {
+            return error(res, 404, "no existe el alumno");
+        }
+
+        await data.destroy()
+        res.status(200)
+        res.json({ ok: true })
     })
 
 export default alumnos
--- api/docentes.js
+++ api/docentes.js
@@ -1,35 +1,53 @@
 import { Router } from "express";
 import { Student, Teacher } from "../db/index.js";
 import { error } from "./common.js";
+import { body, validationResult } from "express-validator";
+
 
 const docentes = Router()
 
 docentes.route("/")
     .get(async (req, res) => {
-        res.json(await Teacher.findAll())
+        res.status(200).json(await Teacher.findAll())
     })
-    .post(async (req, res) => {
-        const data = await Teacher.create(req.body)
-        res.status(201)
-        res.json({ ok: true, id: data.id })
-    })
+    .post([
+        body("name").notEmpty().withMessage("Se requiere nombre"),
+        body("surname").notEmpty().withMessage("Se requiere apellido"),
+        body("email").isEmail().withMessage("Email no es válido"),
+        body("phone").isMobilePhone().withMessage("Teléfono no es válido")
+    ],
+        async (req, res) => {
+            const errors = validationResult(req)
+
+            if (!errors.isEmpty()){
+                return res.status(400).json({ errors: errors.array() });
+            }
+
+            const data = await Teacher.create(req.body)
+            res.status(201).json({ ok: true, id: data.id })
+        })
 
 docentes.route("/:id")
     .get(async (req, res) => {
         const data = await Teacher.findByPk(req.params.id);
         if (data) {
-            res.json(data)
+            res.status(200).json(data)
         } else {
             error(res, 404, "no existe el docente");
         }
     })
     .put(async (req, res) => {
-        const [data] = await Teacher.upsert({...req.body, id: req.params.id})
+        const [data] = await Teacher.upsert({ ...req.body, id: req.params.id })
         res.status(200)
         res.json({ ok: true, id: data.id })
     })
     .delete(async (req, res) => {
         const data = await Teacher.findByPk(req.params.id)
+
+        if (!data) {
+            return error(res, 404, "no existe el docente");
+        }
+
         await data.destroy()
         res.status(200)
         res.json({ ok: true })
